25c859a0648cd88d6abdf72263b2a0f2fde88f0c
Arthur Assuncao <arthur.xx92@gmail.com>
2026-09-15 21:31:33 -0300

    fix: nomes dos avaliadores

--- src/data/data.ts
+++ src/data/data.ts
@@ -6,11 +6,6 @@ export interface Avaliador {
 }
 
 export const AVALIADORES: Avaliador[] = [
-    { nome: "Aline De Sousa Loli", email: "aline.loli@ifsudestemg.edu.br" },
-    {
-        nome: "Ana Paula Lima Dos Santos",
-        email: "ana.santos@ifsudestemg.edu.br",
-    },
     {
         nome: "André Diniz De Oliveira",
         email: "andre.diniz@ifsudestemg.edu.br",
@@ -20,189 +15,12 @@ export const AVALIADORES: Avaliador[] = [
         email: "antonia.barbosa@ifsudestemg.edu.br",
     },
     {
-        nome: "Antonio Henrique Martins De Carvalho",
-        email: "antonio.henrique@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Artur Monteiro Da Costa",
-        email: "artur.costa@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Arturene Maria Lino Carmo",
-        email: "arturene.carmo@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Beatris Cristina Possato",
-        email: "bia.possato@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Bruno Damien Da Costa Paes Jürgensen",
-        email: "bruno.jurgensen@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Carlos Artur Alevato Leal",
-        email: "carlos.leal@ifsudestemg.edu.br",
+        nome: "Arthur Nascimento Assuncao",
+        email: "arthur.assuncao@ifsudestemg.edu.br",
     },
     {
         nome: "Daniele Rodrigues Francisco",
         email: "daniele.francisco@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Débora Magalhães Kirchmair",
-        email: "debora.kirchmair@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Deborah Pereira Domingues",
-        email: "deborah.domingues@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Fernando Paulo Caneschi",
-        email: "fernando.caneschi@ifsudestemg.edu.br",
-    },
-    { nome: "Flavia Calvano", email: "flavia.calvano@ifsudestemg.edu.br" },
-    {
-        nome: "Francilene Barbosa Dos Santos Silva",
-        email: "francilene.silva@ifsudestemg.edu.br",
-    },
-    { nome: "Geisa Martins Soares", email: "geisa.soares@ifsudestemg.edu.br" },
-    {
-        nome: "Gustavo Jose Santiago Rosseti",
-        email: "gustavo.rosseti@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Helton Nonato De Souza",
-        email: "helton.nonato@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Henrique Selani Silva",
-        email: "henrique.silva@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Helvécio Pinto Do Nascimento",
-        email: "helvecio.nascimento@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Izabel Cristina Rodrigues",
-        email: "isabel.rodrigues@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Joao Paulo Albuquerque Kalil",
-        email: "joaopaulo.kalil@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Joicy Pimentel Ferreira",
-        email: "joicy.pimentel@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Karina Modesto Monteiro",
-        email: "karina.monteiro@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Leonardo Amorim De Araujo",
-        email: "leonardo.araujo@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Leonardo Ribeiro Barbosa",
-        email: "leonardo.barbosa@ifsudestemg.edu.br",
-    },
-    { nome: "Liana Krakecker", email: "liana.krakecker@ifsudestemg.edu.br" },
-    {
-        nome: "Lisleandra Machado",
-        email: "lisleandra.machado@ifsudestemg.edu.br",
-    },
-    { nome: "Livia Meneguitte Avila", email: "livia.avila@ifsudestemg.edu.br" },
-    { nome: "Lucas Medeiros E Melo", email: "lucas.melo@ifsudestemg.edu.br" },
-    { nome: "Lucas Mendes Ferreira", email: "lucas.mendes@ifsudestemg.edu.br" },
-    {
-        nome: "Luciano Goncalves Moreira",
-        email: "luciano.moreira@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Marcio De Paiva Delgado",
-        email: "marcio.delgado@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Maycoln Jose De Oliveira",
-        email: "maycoln.oliveira@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Monalisa Reis Da Silva",
-        email: "monalisa.silva@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Pablo Carlos De Siqueira Furtado",
-        email: "pablo.furtado@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Patricia Morais Gomes",
-        email: "patricia.gomes@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Patricia Rafaela Otoni Ribeiro",
-        email: "patricia.otoni@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Paulo Cezar De Oliveira",
-        email: "paulo.oliveira@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Philipe Augusto De Paula Pacheco",
-        email: "philipe.pacheco@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Reginaldo Jose Cavallaro",
-        email: "reginaldo.cavallaro@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Ricardo Facchini Garcia",
-        email: "ricardo.facchini@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Richard Correa Esteves Junior",
-        email: "richard.junior@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Samuel Oliveira De Almeida",
-        email: "samuel.oliveira@ifsudestemg.edu.br",
-    },
-    { nome: "Sandro Farias Pinto", email: "sandro.pinto@ifsudestemg.edu.br" },
-    { nome: "Sarah Munck Vieira", email: "sarah.vieira@ifsudestemg.edu.br" },
-    {
-        nome: "Silvana Rodrigues Pires Moreira",
-        email: "silvana.moreira@ifsudestemg.edu.br",
-    },
-    { nome: "Tadeu Samuel Pereira", email: "tadeu.pereira@ifsudestemg.edu.br" },
-    {
-        nome: "Tiago Favero De Oliveira",
-        email: "tiago.oliveira@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Vicente Rezende De Almeida Junior",
-        email: "vicente.junior@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Wesley Henrique Maciel",
-        email: "wesley.maciel@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Arthur Bernardo Lisboa Webler",
-        email: "arthur.webler@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Breno Apolinário Da Silva",
-        email: "breno.apolinario@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Cristia Rodrigues Miranda",
-        email: "cristia.miranda@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Cristiane Valladares De Azevedo",
-        email: "cristiane.valladares@ifsudestemg.edu.br",
-    },
-    {
-        nome: "Daniele Pires Magalhães",
-        email: "daniele.magalhaes@ifsudestemg.edu.br",
     },
     {
         nome: "Denis Ribeiro Maurício",
@@ -213,16 +31,55 @@ export const AVALIADORES: Avaliador[] = [
         email: "fernanda.oliveira@ifsudestemg.edu.br",
     },
     {
-        nome: "Gislaini Andrezza Santos",
-        email: "gislaini.santos@ifsudestemg.edu.br",
+        nome: "Flavia Calvano",
+        email: "flavia.calvano@ifsudestemg.edu.br",
     },
-    { nome: "Lucas Lucinda De Sá", email: "lucas.sa@ifsudestemg.edu.br" },
+    {
+        nome: "Helvécio Pinto Do Nascimento",
+        email: "helvecio.nascimento@ifsudestemg.edu.br",
+    },
+    {
+        nome: "Leonardo Amorim De Araujo",
+        email: "leonardo.araujo@ifsudestemg.edu.br",
+    },
     {
         nome: "Marcelo Gomes Da Silva",
         email: "marcelo.gomes@ifsudestemg.edu.br",
     },
     {
+        nome: "Marcio De Paiva Delgado",
+        email: "marcio.delgado@ifsudestemg.edu.br",
+    },
+    {
+        nome: "Monalisa Reis Da Silva",
+        email: "monalisa.silva@ifsudestemg.edu.br",
+    },
+    {
+        nome: "Patricia Rafaela Otoni Ribeiro",
+        email: "patricia.otoni@ifsudestemg.edu.br",
+    },
+    {
+        nome: "Reginaldo Jose Cavallaro",
+        email: "reginaldo.cavallaro@ifsudestemg.edu.br",
+    },
+    {
+        nome: "Richard Correa Esteves Junior",
+        email: "richard.junior@ifsudestemg.edu.br",
+    },
+    {
+        nome: "Silvana Rodrigues Pires Moreira",
+        email: "silvana.moreira@ifsudestemg.edu.br",
+    },
+    {
+        nome: "Tadeu Samuel Pereira",
+        email: "tadeu.pereira@ifsudestemg.edu.br",
+    },
+    {
         nome: "Tiago Rodrigues Moreira",
         email: "tiago.moreira@ifsudestemg.edu.br",
+    },
+    {
+        nome: "Tiago Favero De Oliveira",
+        email: "tiago.oliveira@ifsudestemg.edu.br",
     },
 ];
