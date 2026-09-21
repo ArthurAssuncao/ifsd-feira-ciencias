a6ad8e932df37dc86aa54f9ff1c1ae3cd25cf999
Arthur Assuncao <arthur.xx92@gmail.com>
2026-09-21 12:37:06 -0300

    add: alguns avaliadores

--- src/components/DadosAvaliadorSection.tsx
+++ src/components/DadosAvaliadorSection.tsx
@@ -21,6 +21,12 @@ export function DadosAvaliadorSection({
         toTitleCase(av.nome, true),
     );
     const emailsAvaliadores = AVALIADORES.map((a: Avaliador) => a.email);
+    const trabalhosAvaliadosDoAvaliador = AVALIADORES.find(
+        (a: Avaliador) => a.email === emailAvaliador,
+    );
+    const trabalhosAvaliados = trabalhosAvaliadosDoAvaliador
+        ? trabalhosAvaliadosDoAvaliador.projetosAvaliados
+        : [];
 
     return (
         <div className="space-y-4">
@@ -41,6 +47,18 @@ export function DadosAvaliadorSection({
                 placeholder="seu.nome@email.com"
                 required
             />
+
+            <div className="flex flex-wrap gap-2 items-center ">
+                <span className="text-xs text-gray-500">Grupos indicados:</span>
+                {trabalhosAvaliados.map((t: number) => (
+                    <span
+                        key={t}
+                        className="inline-flex items-center justify-center rounded-full bg-green-100 px-3 py-2 text-xs font-semibold text-green-700 ring-1 ring-inset ring-green-200 leading-none"
+                    >
+                        G{t}
+                    </span>
+                ))}
+            </div>
         </div>
     );
 }
--- src/data/data.ts
+++ src/data/data.ts
@@ -3,83 +3,118 @@ export const MAX_NOTA = 10;
 export interface Avaliador {
     nome: string;
     email: string;
+    projetosAvaliados: number[];
 }
 
 export const AVALIADORES: Avaliador[] = [
     {
         nome: "André Diniz De Oliveira",
         email: "andre.diniz@ifsudestemg.edu.br",
+        projetosAvaliados: [10, 13, 34, 39, 42],
     },
     {
         nome: "Antonia Amelia Barbosa",
         email: "antonia.barbosa@ifsudestemg.edu.br",
+        projetosAvaliados: [6, 28, 29, 41],
     },
     {
         nome: "Arthur Nascimento Assuncao",
         email: "arthur.assuncao@ifsudestemg.edu.br",
+        projetosAvaliados: [6, 14, 22, 42, 45],
+    },
+    {
+        nome: "Arturene Maria Lino Carmo",
+        email: "arturene.carmo@ifsudestemg.edu.br",
+        projetosAvaliados: [], // Não constava na lista de atribuições
+    },
+    {
+        nome: "Breno Apolinário da Silva",
+        email: "breno.apolinario@ifsudestemg.edu.br",
+        projetosAvaliados: [2, 11, 27, 28, 31],
     },
     {
         nome: "Daniele Rodrigues Francisco",
         email: "daniele.francisco@ifsudestemg.edu.br",
+        projetosAvaliados: [3, 7, 25, 26, 44],
     },
     {
         nome: "Denis Ribeiro Maurício",
         email: "denis.ribeiro@ifsudestemg.edu.br",
+        projetosAvaliados: [5, 9, 15, 20, 45],
     },
     {
         nome: "Fernanda Do Nascimento Oliveira",
         email: "fernanda.oliveira@ifsudestemg.edu.br",
+        projetosAvaliados: [13, 17, 31, 37, 40],
     },
     {
         nome: "Flavia Calvano",
         email: "flavia.calvano@ifsudestemg.edu.br",
+        projetosAvaliados: [1, 7, 26],
     },
     {
         nome: "Helvécio Pinto Do Nascimento",
         email: "helvecio.nascimento@ifsudestemg.edu.br",
+        projetosAvaliados: [21, 24, 30],
     },
     {
         nome: "Leonardo Amorim De Araujo",
         email: "leonardo.araujo@ifsudestemg.edu.br",
+        projetosAvaliados: [12, 20, 46],
     },
     {
         nome: "Marcelo Gomes Da Silva",
         email: "marcelo.gomes@ifsudestemg.edu.br",
+        projetosAvaliados: [3, 14, 19, 41],
     },
     {
         nome: "Marcio De Paiva Delgado",
         email: "marcio.delgado@ifsudestemg.edu.br",
+        projetosAvaliados: [4, 16, 35],
     },
     {
         nome: "Monalisa Reis Da Silva",
         email: "monalisa.silva@ifsudestemg.edu.br",
+        projetosAvaliados: [1, 11, 44],
     },
     {
         nome: "Patricia Rafaela Otoni Ribeiro",
         email: "patricia.otoni@ifsudestemg.edu.br",
+        projetosAvaliados: [15, 18, 33, 35],
     },
     {
         nome: "Reginaldo Jose Cavallaro",
         email: "reginaldo.cavallaro@ifsudestemg.edu.br",
+        projetosAvaliados: [5, 12, 27, 38, 40],
     },
     {
         nome: "Richard Correa Esteves Junior",
         email: "richard.junior@ifsudestemg.edu.br",
+        projetosAvaliados: [9, 10, 34, 37, 38],
+    },
+    {
+        nome: "Rodrigo Carlos da Silva",
+        email: "rodfisicaufjf@gmail.com",
+        projetosAvaliados: [18, 22, 25, 32, 43],
     },
     {
         nome: "Silvana Rodrigues Pires Moreira",
         email: "silvana.moreira@ifsudestemg.edu.br",
+        projetosAvaliados: [4, 8, 16, 36, 43],
     },
     {
         nome: "Tadeu Samuel Pereira",
         email: "tadeu.pereira@ifsudestemg.edu.br",
+        projetosAvaliados: [8, 19, 29, 39, 46],
     },
     {
         nome: "Tiago Rodrigues Moreira",
         email: "tiago.moreira@ifsudestemg.edu.br",
+        projetosAvaliados: [17, 21, 24, 32, 36],
     },
     {
         nome: "Tiago Favero De Oliveira",
         email: "tiago.oliveira@ifsudestemg.edu.br",
+        projetosAvaliados: [2, 30, 33],
     },
 ];
